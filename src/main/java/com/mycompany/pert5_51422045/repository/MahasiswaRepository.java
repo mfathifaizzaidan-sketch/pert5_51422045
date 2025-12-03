c34b9a08ccbf2aad867d1997901aa4e99acb9e0c
unknown <mfathifaizzaidan@gmail.com>
2025-12-03 22:29:10 +0700

    First Commit

---
+++ src/main/java/com/mycompany/pert5_51422045/repository/MahasiswaRepository.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pert5_51422045.repository;
+
+import com.mycompany.pert5_51422045.model.ModelMahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+/**
+ *
+ * @author user
+ */
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Long> {
+
+}
+
